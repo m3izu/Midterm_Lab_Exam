ec88a26a348276f36f00482467d025e344ce4524
m3izu <rfsgalendez@gmail.com>
2024-04-24 15:50:23 +0800

    Update 3.cpp
    naay mali

--- 3.cpp
+++ 3.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     int rows;
     cout << "Enter the number of rows for the pyramid: ";
-    cin > rows;
+    cin >> rows;
 
     for (int i = 1; i <= rows; ++i) {
         for (int j = 1; j <= rows - i; ++j) {
