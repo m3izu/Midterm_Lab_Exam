d4861d9469200d76cf6678b12ee4769e84e820ad
m3izu <rfsgalendez@gmail.com>
2024-04-24 15:49:30 +0800

    Update 3.cpp
    finished

--- 3.cpp
+++ 3.cpp
@@ -1,5 +1,19 @@
-#include <iostream>
+##include <iostream>
 using namespace std;
 
 int main() {
-    int rows
+    int rows;
+    cout << "Enter the number of rows for the pyramid: ";
+    cin > rows;
+
+    for (int i = 1; i <= rows; ++i) {
+        for (int j = 1; j <= rows - i; ++j) {
+            cout << " ";
+        }
+        for (int k = 1; k <= i; ++k) {
+            cout << k << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
