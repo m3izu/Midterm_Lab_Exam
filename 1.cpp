2cb7fa9a83d12f1756ad845cdb09f6b62e18beb5
m3izu <rfsgalendez@gmail.com>
2024-04-24 15:14:51 +0800

    Update 1.cpp
    fix typos

--- 1.cpp
+++ 1.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 using namespace std;
 
-int maim() {
-    fr (char c = 'a'; c <= 'z'; ++c) {
+int main() {
+    for (char c = 'a'; c <= 'z'; ++c) {
         cout << c << " ";
     }
     return 0;
